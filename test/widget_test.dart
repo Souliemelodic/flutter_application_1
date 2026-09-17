8c3260591e163cd5ef25ec165aa388b98ad43c04
Indi <indibrown08@gmail.com>
2026-09-17 16:51:24 -0500

    testing

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -100,16 +100,16 @@ testWidgets('4 divided by 2', (WidgetTester tester) async {
       '2',
     );
   });
-testWidgets('20 divided by 4', (WidgetTester tester) async {
+testWidgets('6 divided by 2', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
-    await tester.tap(find.text('20'));
+    await tester.tap(find.text('6'));
     await tester.tap(find.text('/'));
-    await tester.tap(find.text('4'));
+    await tester.tap(find.text('2'));
     await tester.tap(find.text('='));
     await tester.pump();
     expect(
       tester.widget<Text>(find.byKey(const Key('calculator-display'))).data,
-      '5',
+      '3',
     );
   });
 
